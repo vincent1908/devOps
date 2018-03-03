5199de05288be629e6ca5b53b74665e0cfbe56d3
gregory vincent <softtester19@gmail.com>
2018-03-03 18:57:33 +0530

    1

---
+++ src/test/java/test/program/FibonacciTest.java
@@ -0,0 +1,18 @@
+package test.program;
+
+public class FibonacciTest {
+
+	public static void main(String[] args) {
+
+		int a = 0, b = 1, c, count=10;
+		System.out.print(a+" "+b+" ");
+		
+		for(int i = 2; i<count;i++){
+			c=a+b;
+			System.out.print(c+" ");
+			a=b;
+			b=c;
+		}
+	}
+
+}

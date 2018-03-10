4c8806c4743387f13a154217b685e0b846d18b03
gamirtha <gamirtha@LIN16009221.corp.capgemini.com>
2018-03-10 13:07:16 +0530

    1

--- src/test/java/test/program/PrimeNumber.java
+++ src/test/java/test/program/PrimeNumber.java
@@ -2,10 +2,24 @@ package test.program;
 
 public class PrimeNumber {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		int num = 29;
+
+		// 2 29/2 ++
+		// num%i
+
+		// System.out.println(29%1);
+
+		for (int i = 2; i <= num / 2; i++) {
+			if (num % i == 0)
+				System.out.println(i);
+				break;
+		}
 		
+		
+
 	}
 
 }
